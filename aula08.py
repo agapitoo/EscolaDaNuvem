3a03e2dd2665207fd8eed17cca258627ab9b08c5
agapitoo <fernandaagapito2013@gmail.com>
2025-03-14 20:37:32 -0300

    gerador de senhas

---
+++ aula08.py
@@ -0,0 +1,14 @@
+# crie um programa que gere uma senha aleatoria usando o randon 
+
+import random
+import string
+
+def gerar_senha(tamanho=8):
+    caracteres = string.ascii_letters + string.digits + '!@#$%&*'
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+tamanho_senha = int(input("Digite o tamanho da senha desejada: "))
+
+senha_gerada = gerar_senha(tamanho_senha)
+print(f"Senha gerada: {senha_gerada}")
